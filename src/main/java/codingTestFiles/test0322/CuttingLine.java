23494e289f75d6f797d4ca528da0027d21f717c7
Ham Geonwook <103511161+tomy8964@users.noreply.github.com>
2023-05-13 20:46:21 +0900

    Update CuttingLine.java

--- src/main/java/codingTestFiles/test0322/CuttingLine.java
+++ src/main/java/codingTestFiles/test0322/CuttingLine.java
@@ -35,14 +35,6 @@ public class CuttingLine {
             for (int i = 0; i < arr.length; i++) {
                 count += (arr[i] / mid);
             }
-
-            /*
-             *  [upper bound 형식]
-             *
-             *  mid 길이로 잘랐을 때의 개수가 만들고자 하는 랜선의 개수보다 작다면
-             *  자르고자 하는 길이를 줄이기 위해 최대 길이를 줄인다.
-             *  그 외에는 자르고자 하는 길이를 늘려야 하므로 최소 길이를 늘린다.
-             */
             if(count < m) {
                 max = mid;
             }
