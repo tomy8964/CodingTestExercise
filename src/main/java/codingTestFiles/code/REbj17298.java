8f1d4eaaed7e00a719af609a59bf50f3dfe3639c
ham <tomy8964@naver.com>
2023-11-20 15:23:45 +0900

    Update REbj17298.java

--- src/main/java/codingTestFiles/code/REbj17298.java
+++ src/main/java/codingTestFiles/code/REbj17298.java
@@ -7,30 +7,40 @@ import java.io.OutputStreamWriter;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class bj17298 {
+public class REbj17298 {
 
     public static void main(String[] args) throws Exception {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
         Stack<Integer> stack = new Stack<>();
-        int[] answer = new int[N];
+        int[] result = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            if (!stack.empty()) {
-                Integer peek = stack.peek();
-                if (peek < num) {
-                    stack.pop();
-                    stack.push(num);
-                    answer[i-1] = num;
-                } else stack.push(num);
-            } else stack.push(num);
 
+        for (int i = 0; i < N; i++) {
+            String line = st.nextToken();
+            arr[i] = Integer.parseInt(line);
         }
 
-        System.out.println(stack);
+        stack.push(0);
+
+        for (int i = 1; i < N; i++) {
+            while(!stack.isEmpty()&&(arr[stack.peek()] < arr[i])){
+                    result[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            result[stack.pop()] = -1;
+        }
+
+        for(int i=0; i<result.length; i++) {
+            bw.write(result[i] + " ");
+        }
+        bw.write("\n");
 
         bw.flush();
         bw.close();
