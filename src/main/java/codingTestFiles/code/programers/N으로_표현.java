facb9e3906618e4e53205bc1a7a379bea8bc4015
ham <tomy8964@naver.com>
2024-05-16 21:33:36 +0900

    Create N으로_표현.java

---
+++ src/main/java/codingTestFiles/code/programers/N으로_표현.java
@@ -0,0 +1,64 @@
+package codingTestFiles.code.programers;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class N으로_표현 {
+
+    public static void main(String[] args) {
+        // N	number	return
+        // 5	12	    4
+        // 2	11	    3
+        System.out.println(new N으로_표현().solution(5, 12));
+        System.out.println(new N으로_표현().solution(2, 11));
+    }
+
+    public int solution(int N, int number) {
+        int answer = -1;
+
+        // N을 몇개 사용해서 만들 수 있는 숫자들로 분리
+        // dp[1] == N을 1개 사용해서 만들 수 있는 숫자
+        // dp[2] == dp[1] ( +, -, *, /) dp[1] + NN
+        // dp[3] == dp[1] ( +, -, *, /) dp[2] + dp[2] ( +, -, *, /) dp[1] + NNN
+        // dp[4] == dp[1] ( +, -, *, /) dp[3] + dp[3] ( +, -, *, /) dp[1] + dp[2] ( +, -, *, /) dp[2] + NNNN
+
+        List<Set<Integer>> setList = new ArrayList<>();
+
+        // 9 개 이상의 숫자를 사용하면 -1를 리턴 : 문제 조건
+        for (int i = 0; i < 9; i++) {
+            setList.add(new HashSet<>());
+        }
+
+        // N을 1개를 사용해서 만들 수 있는 숫자는 N 하나뿐
+        setList.get(1).add(N);
+
+        if (N == number) return 1;
+
+        for (int i = 2; i < 9; i++) {
+            Set<Integer> numbers = setList.get(i);
+            for (int j = 1; j <= i; j++) {
+                Set<Integer> firstSet = setList.get(j);
+                Set<Integer> secondSet = setList.get(i - j);
+                for (Integer first : firstSet) {
+                    for (Integer second : secondSet) {
+                        numbers.add(first + second);
+                        numbers.add(first - second);
+                        numbers.add(first * second);
+                        if (first != 0 && second != 0) {
+                            numbers.add(first / second);
+                        }
+                    }
+                }
+            }
+            numbers.add(Integer.parseInt(String.valueOf(N).repeat(i)));
+        }
+
+        for (Set s : setList) {
+            if (s.contains(number)) return setList.indexOf(s);
+        }
+
+        return answer;
+    }
+}
